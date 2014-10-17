d6acf6d331b0fbcf853dd42554aa2626c2c6d374
Mark Wilson <secretchinamark@gmail.com>
2014-10-17 13:48:44 +0800

    Show a random item of each cuisine on the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @western_item = FoodItem.where(cuisine: "Western").sample
+    @asian_item = FoodItem.where(cuisine: "Asian").sample
+    @adventurous_item = FoodItem.where(cuisine: "Adventurous").sample
   end
 end
